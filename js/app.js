90c2c3452d136cb80511aa3715f5cd07b49140f4
Bibo <sabinehuschke@googlemail.com>
2018-08-29 11:20:20 +0200

    ..

--- js/app.js
+++ js/app.js
@@ -258,8 +258,8 @@ $(".next-link").click(function () {
 			content:
 			'<div class="popup" id="popupStart">' +
 				  '<div class="view">' +
-					'<div class="page">' +
-					  '<div class="navbar">' +
+					'<div class="page popupStartpage">' +
+					  '<div class="navbar popupNavbar">' +
 						'<div class="navbar-inner">' +
 						  '<div class="title">Popup</div>' +
 						  '<div class="right">' +
