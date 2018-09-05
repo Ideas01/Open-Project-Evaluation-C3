f7646b122e56acd5324058a5ccd5c27c94007bd4
Bibo <sabinehuschke@googlemail.com>
2018-09-05 11:37:29 +0200

    animation popup
    für weiterleitung zum puzzle

--- js/app.js
+++ js/app.js
@@ -123,7 +123,21 @@ app.on('pageInit', function(page){
 							'</div>' +
 							'<div class="page-content">' +
 								'<div class="block">' +
-									'<p>Vielen Dank! Du hast dir alle Seiten des Prototypen angeschaut. </p>' +
+									'<p>Danke für deine persönliche Bewertung! Du wirst nun zum Puzzlespiel weitergeleitet. </p>' +
+                                         '<div class="sk-circle">' +
+                                         '<div class="sk-circle1 sk-child"></div>' +
+                                         '<div class="sk-circle2 sk-child"></div>' +
+                                         '<div class="sk-circle3 sk-child"></div>' +
+                                         '<div class="sk-circle4 sk-child"></div>' +
+                                         '<div class="sk-circle5 sk-child"></div>' +
+                                         '<div class="sk-circle6 sk-child"></div>' +
+                                         '<div class="sk-circle7 sk-child"></div>' +
+                                         '<div class="sk-circle8 sk-child"></div>' +
+                                         '<div class="sk-circle9 sk-child"></div>' +
+                                         '<div class="sk-circle10 sk-child"></div>' +
+                                         '<div class="sk-circle11 sk-child"></div>' +
+                                         '<div class="sk-circle12 sk-child"></div>' +
+                                         '</div>' +
 									'<div class="next" text-align="center">' +
 										'<a href="/puzzle/" class="button popup-close"> Weiter </a>' +
 									'</div>' +
