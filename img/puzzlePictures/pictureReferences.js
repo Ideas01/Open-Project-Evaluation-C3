71eca19fbc1b9db5e76c74b1e31bf9783cdfe332
Cake <carina.kessel@smail.th-koeln.de>
2018-10-04 23:21:10 +0200

    quellen ergänzt
    Quellen von dianas bildern ergänzt

--- img/puzzlePictures/pictureReferences.js
+++ img/puzzlePictures/pictureReferences.js
@@ -1,95 +1,96 @@
 
 var pictureURLs = {
 data: [{
+	//Bildquelle: https://unsplash.com/photos/b079C-_tUbM
 	url: "img/puzzlePictures/cat.jpg",
 	correctAnswer: "Katze",
 	category: "Tiere",
 	wrongAnswers: ["Tiger", "Waschbär", "Hamster", "Vogel", "Kaninchen"]
 	
 },{
+	//Bildquelle: https://www.fellomed.de/wp-content/uploads/2018/04/Nasenbluten_Hund.jpg
     url: "img/puzzlePictures/hund.jpg",
     correctAnswer: "Hund",
     category: "Tiere",
     wrongAnswers: ["Katze", "Maus", "Bär", "Pferd", "Esel"]
 
 },{
+	//Bildquelle: https://dissolve.com/stock-photo/Germany-Berlin-Brandenburg-Gate-royalty-free-image/101-D1089-27-143
     url: "img/puzzlePictures/berlin.jpg",
     correctAnswer: "Berlin",
     category: "Städte",
     wrongAnswers: ["Hamburg", "Venedig", "Paris", "London", "Köln"]
 
 },{
-	url: "img/puzzlePictures/berlin.jpg",
-	correctAnswer: "Berlin",
-	category: "Städte",
-	wrongAnswers: ["Hamburg", "Venedig", "Paris", "London", "Köln"]
-	
-},{
+	//Bildquelle: https://1001reisetipps.de/wp-content/uploads/2017/10/paris-2643595_1920-1140x760.jpg
     url: "img/puzzlePictures/paris.jpg",
     correctAnswer: "Paris",
     category: "Städte",
     wrongAnswers: ["Hamburg", "Venedig", "Berlin", "London", "Köln"]
 
 },{
+	//Bildquelle: https://unsplash.com/photos/Yn9LiXt53jQ
     url: "img/puzzlePictures/london.jpg",
     correctAnswer: "London",
     category: "Städte",
     wrongAnswers: ["Hamburg", "Venedig", "Paris", "New York", "Köln"]
 
 },{
+	//Bildquelle: https://unsplash.com/photos/eSeo6IzOV00
     url: "img/puzzlePictures/pizza2.jpg",
     correctAnswer: "Pizza",
     category: "Essen",
     wrongAnswers: ["Lasagne", "Spagetti", "Salat", "Döner", "Kuchen"]
 
 },{
+	//Bildquelle: http://pacificlanding.ca/wp-content/uploads/2018/05/jez-timms-634288-unsplash-800x430.jpg
     url: "img/puzzlePictures/salat2.jpg",
     correctAnswer: "Salat",
     category: "Essen",
     wrongAnswers: ["Lasagne", "Pizza", "Spagetti", "Döner", "Sushi"]
 
 },{
+	//Bildquelle: https://www.anotherworldadventures.com/unusual-adventure-holidays/drc-virunga-trekking-expedition/
     url: "img/puzzlePictures/gorilla.jpg",
     correctAnswer: "Gorilla",
     category: "Tiere",
     wrongAnswers: ["Affe", "Bär", "Pandabär", "Jaguar", "Schlange"]
 
 },{
+	//Bildquelle: https://unsplash.com/photos/5xNhXCAhSbk
     url: "img/puzzlePictures/elefant.jpg",
     correctAnswer: "Elefant",
     category: "Tiere",
     wrongAnswers: ["Nashorn", "Nielpferd", "Krokodil", "Dinosaurier", "Wal"]
 
 },{
+	//Bildquelle: https://gedankenwelt.de/hoffnung-antrieb-und-inspiration-unserer-wuensche/#
     url: "img/puzzlePictures/löwenzahn.jpg",
     correctAnswer: "Löwenzahn",
     category: "Blumen",
     wrongAnswers: ["Rose", "Sonnenblume", "Veilchen", "Gänseblümchen", "Mohnblume"]
 
 },{
-    url: "img/puzzlePictures/daisyflower2.jpg",
+	//Bildquelle: http://www.galloimages.co.za/image/single-daisy-flower-patch-green-grass/518417162/2
+    url: "img/puzzlePictures/daisyflower.jpg",
     correctAnswer: "Gänseblümchen",
     category: "Blumen",
     wrongAnswers: ["Rose", "Sonnenblume", "Mohnblume", "Löwenzahn", "Veilchen"]
 
 },{
+	//Bildquelle: https://unsplash.com/photos/PSiRCs5-_QE
     url: "img/puzzlePictures/sunflower.jpg",
     correctAnswer: "Sonnenblume",
     category: "Blumen",
     wrongAnswers: ["Rose", "Löwenzahn", "Veilchen", "Gänseblümchen", "Mohnblume"]
 
 },{
+	//Bildquelle: http://clubd.net/887-monkey-on-the-tree-in-the-zoo-94.html
 	url: "img/puzzlePictures/affe.jpg",
 	correctAnswer: "Affe",
 	category: "Tiere",
 	wrongAnswers: 
 		["Gorilla", "Bär", "Hund", "Esel", "Eichhörnchen"]
 	
-},{
-    url: "img/puzzlePictures/arzt.jpg",
-    correctAnswer: "Arzt",
-    category: "Berufe",
-    wrongAnswers: ["Koch", "Handwerker", "Lehrer", "Kassierer", "Gärtner"]
-
-},]
+}]
 }
